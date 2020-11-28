9411c2955efbb40ecb8bedc636f0716ffd4fd04b
YohanAquino <74798062+YohanAquino@users.noreply.github.com>
2020-11-27 21:43:17 -0300

    Add files via upload

---
+++ EX-11-LISTA-1-Yohan_Aquino/EX-11-LISTA-1-Yohan_Aquino/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX_11_LISTA_1_Yohan_Aquino
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*12. A partir de três valores que serão digitados, verificar se formam ou não um triângulo. Em
+caso positivo, exibir sua classificação: “Isósceles, escaleno ou eqüilátero”. Um triângulo escaleno
+possui todos os lados diferentes, o isósceles, dois lados iguais e o eqüilátero, todos os lados
+iguais. Para existir triângulo é necessário que a soma de dois lados quaisquer seja maior que o
+outro, isto, para os três lados. */
+            Triangulo tri = new Triangulo();
+
+            Console.WriteLine("Digite o 1°valor");
+            tri.setNum1(int.Parse(Console.ReadLine()));
+
+            Console.WriteLine("Digite o 2°valor");
+            tri.setNum2(int.Parse(Console.ReadLine()));
+
+            Console.WriteLine("Digite o 3°valor");
+            tri.setNum3(int.Parse(Console.ReadLine()));
+
+            if (tri.verificarTriangulo()) 
+            {
+                Console.WriteLine(tri.tipoTriangulo());
+            }
+            else 
+            {
+                Console.WriteLine("Não forma um triangulo");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ EX-11-LISTA-1-Yohan_Aquino/EX-11-LISTA-1-Yohan_Aquino/Triangulo.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX_11_LISTA_1_Yohan_Aquino
+{
+    class Triangulo
+    {
+        private int n1;
+        private int n2;
+        private int n3;
+
+        public void setNum1(int x) 
+        {
+            n1 = x;
+        }
+        public void setNum2(int x)
+        {
+            n2 = x;
+        }
+        public void setNum3(int x)
+        {
+            n3 = x;
+        }
+        public Boolean verificarTriangulo() 
+        {
+            if (n1 < (n2 + n3) && n2 < (n1 + n3) && n3 < (n1 + n2))
+            {
+                return true;
+            }
+            else 
+            {
+                return false;
+            }
+            }
+
+        public string tipoTriangulo() 
+        {
+            if (n1 == n2 && n1 == n3 && n2 == n3)
+            {
+               return "Os três valores formam um triângulo equilátero";
+            }
+            else
+            {
+                if (n1 != n2 && n1 != n3 && n2 != n3)
+                {
+                    return "Os três valores formam um triângulo escaleno";
+                }
+                else
+                {
+                    return "Os três valores formam um triângulo isósceles";
+                }
+            }
+        }
+
+    }
+}
